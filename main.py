8f99ca962b18a6dd3db495926d8086da8fd32fc8
José Amauri <amauri.patrico13@gmail.com>
2025-04-19 10:00:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import random as rd
-palavra = ['Futebol','Volêi', 'tenis', 'Basqute']
+palavra = ['melao','mamao', 'amora', 'limao']
 palavra_escolhida = rd.choice(palavra)
+print(palavra_escolhida)
+print('Olá, vamo jogar um jogo da forca')
+print('2 dicas é uma fruta e a palavra possui 5 letras')
+letra = input('Digite uma letra para sua primeira tentativa:')
+letra.lower()
+letras_palavra = '- - - - -'
+letras_palavra.split(' ')
+print(letras_palavra)
+i = palavra_escolhida.index(letra)
+print(i)
 
+letras_palavra[i] = letra
+print(letras_palavra)
+
+    
