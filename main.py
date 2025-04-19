6f1e7fa3178bb2bdb27e9337fca8cccae799c072
José Amauri <amauri.patrico13@gmail.com>
2025-04-19 12:01:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 import random as rd
-palavra = ['melao','mamao', 'amora', 'limao']
+palavra = ['melao','mamao', 'amora', 'limao','manga','uvaia']
 palavra_escolhida = rd.choice(palavra)
-print(palavra_escolhida)
+
 print('Olá, vamo jogar um jogo da forca')
 print('2 dicas é uma fruta e a palavra possui 5 letras')
-letra = input('Digite uma letra para sua primeira tentativa:')
-letra.lower()
-letras_palavra = '- - - - -'
-letras_palavra.split(' ')
-print(letras_palavra)
-i = palavra_escolhida.index(letra)
-print(i)
-
-letras_palavra[i] = letra
-print(letras_palavra)
+letras_palavra = ['-', '-', '-', '-', '-']
+print(''.join(letras_palavra))
+print('Vamos lá, você tem 6 tentativas se você acertar a letra você ganha a opção de chute, a cada tentativa errada você será informado, se você utilizar das suas 6 tentativas e não acertar você ganha orpotunidade de chutar tambem.')
+for i in range(6):
+    i += 1
+    letra = input('Digite uma letra para sua {}º tentativa:'.format(i))
+    letra =letra.lower()
+    if letra in palavra_escolhida:
+        for index, char in enumerate(palavra_escolhida):
+            if char == letra: 
+                letras_palavra[index] = letra
+        if ''.join(letras_palavra) == palavra_escolhida:
+            print ('Você acertou a palavra!! fim do game')
+            break
+        else:
+            pass
+        print(''.join(letras_palavra))
+        print('Parabéns você acertou uma letra')
+        opc = input('Digite S de sim para chutar ou digite N de não para ir para proxima tentativa:')
+        opc =opc.upper()
+        if opc == 'S':
+            chute = input('Sabe a palavra, vamos lá diga:')
+            if chute == palavra_escolhida:
+                print('Parabéns você acertou, a palavra era {} e voce acertou na {}º tentativa, incrivel'.format(palavra_escolhida, i))
+                break
+            else:
+                print('Você errou, vamos para a proxima')
+        elif opc == 'N':
+            if i < 6:
+                print('Certo então vamos para a proxima tentativa')
+            else:
+                 pass
+        else:
+            print('opção invalida:')
+                 
 
     
